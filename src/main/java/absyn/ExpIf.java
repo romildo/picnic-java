48e2d31e82a620b2f5b91fcc7dd86986846c6c0c
José Romildo Malaquias <malaquias@gmail.com>
2018-11-07 13:37:06 -0200

    Add AST for conditional expression

---
+++ src/main/java/absyn/ExpIf.java
@@ -0,0 +1,24 @@
+package absyn;
+
+import io.vavr.collection.Tree;
+
+public class ExpIf extends Exp {
+
+   public final Exp test;
+   public final Exp alt1;
+   public final Exp alt2;
+
+   public ExpIf(Exp test, Exp alt1, Exp alt2) {
+      this.test = test;
+      this.alt1 = alt1;
+      this.alt2 = alt2;
+   }
+
+   @Override
+   public Tree.Node<String> toTree() {
+      return Tree.of("ExpIf",
+                     test.toTree(),
+                     alt1.toTree(),
+                     alt2.toTree());
+   }
+}
