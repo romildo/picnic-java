acfc39ace63d99e659b744fb69670a4eb69d3dc0
José Romildo Malaquias <malaquias@gmail.com>
2018-11-19 13:22:38 -0200

    Rename ExpNum to ExpInt

--- src/main/java/absyn/ExpInt.java
+++ src/main/java/absyn/ExpInt.java
@@ -5,18 +5,18 @@ import io.vavr.collection.Tree;
 import types.INT;
 import types.Type;
 
-public class ExpNum extends Exp {
+public class ExpInt extends Exp {
 
    public final long value;
 
-   public ExpNum(Loc loc, String value) {
+   public ExpInt(Loc loc, String value) {
       super(loc);
       this.value = new Long(value);
    }
 
    @Override
    public Tree.Node<String> toTree() {
-      return Tree.of(annotateType("ExpNum: " + value));
+      return Tree.of(annotateType("ExpInt: " + value));
    }
 
    @Override
