e8c74cd08a48d57afd10e5d141548e926ea1d950
José Romildo Malaquias <malaquias@gmail.com>
2018-11-19 06:49:57 -0200

    Add classes to represent data for variables and functions in a symbol table.
    This is needed because variable and function names share the same symbol table.

---
+++ src/main/java/env/VarEntry.java
@@ -0,0 +1,19 @@
+package env;
+
+import types.Type;
+
+public class VarEntry extends Entry {
+
+   public final Type type;
+
+   public VarEntry(Type type) {
+      this.type = type;
+   }
+
+   @Override
+   public String toString() {
+      return "VarEntry{" +
+         "type=" + type +
+         '}';
+   }
+}
